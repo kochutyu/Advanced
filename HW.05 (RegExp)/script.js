8584a88c613eb2a20e297a9ee2f315cebd77f9c9
kochutyura <kochutyurawork@gmail.com>
2019-10-16 03:03:42 +0300

    RegExp

---
+++ HW.05 (RegExp)/script.js
@@ -0,0 +1,17 @@
+// task 1
+console.log('HTML-color:', /^\#\d{6}$/.test('#000000'));
+
+// task 2
+const promptFL = prompt('Write your first and last name')
+const replace = /( |^)[A-Za-z]/ig;
+console.log('replace Z:', promptFL.replace(replace, ' Z'));
+
+// task 3
+const promptPas = prompt('Write your passport data')
+const checkPassport = /[А-Я]{2}\d{6}/gi;
+console.log('pasport:', checkPassport.test(promptPas) );
+
+// task 4
+const promptNum = prompt('Write your passport data')
+const checkNum = /([0-9]{3}\-[0-9]{3}\-[0-9]{2}\-[0-9]{2})|(\+[0-9]{5}\-[0-9]{3}\-[0-9]{2}\-[0-9]{2})/gi;
+console.log('number:', checkNum.test(promptNum) );
