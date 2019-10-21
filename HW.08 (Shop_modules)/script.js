3b334356160b152e74535a2cda21849b6276bbc9
kochutyura <kochutyurawork@gmail.com>
2019-10-21 17:40:37 +0300

    add new file

--- HW.08 (Shop_modules)/script.js
+++ HW.08 (Shop_modules)/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 let gs = sel => document.querySelector(sel);
+let gsAll = sel => document.querySelectorAll(sel);
 
-let shop = (function () {
+let shopResources = (function () {
     let balance = 3000;
     let products = {
         beer: {
@@ -31,8 +32,42 @@ let shop = (function () {
     }
 
     return {
-        init: init
+        init: init,
+        products: products
     }
-}())
+}());
+shopResources.init()
 
-shop.init()
+let shop = (function () {
+    let amount = 0;
+    function radio() {
+        let radios = document.querySelectorAll('.selChange');
+        for (let i = 0; i < radios.length; i++) {
+        
+            if (radios[i].checked) {
+                let selectProduct = gsAll('label')[i].textContent;
+                return selectProduct;
+            }
+
+        }
+    }
+
+    function count() {
+        if(gs('.enter_count_product').value!='') return gs('.enter_count_product').value;
+        else alert('Value is invalid');
+    }
+
+    gs('.add').onclick = function () {
+        let selected = radio();
+        let countProduct = count();
+        if(gs('.enter_count_product').value!=''){
+            amount += +countProduct; 
+            console.log(amount);
+            gs('.textarea_for_buy').innerHTML += `<span>${selected}: ${countProduct} шт.</span></br>`
+            gs('.enter_count_product').value='';
+            
+        }
+    }
+    
+    return
+}());
