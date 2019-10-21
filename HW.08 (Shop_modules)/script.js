948aca8c106504f44e34e382cbe5bd87778d09f7
kochutyura <kochutyurawork@gmail.com>
2019-10-21 17:53:17 +0300

    add new info

--- HW.08 (Shop_modules)/script.js
+++ HW.08 (Shop_modules)/script.js
@@ -39,11 +39,15 @@ let shopResources = (function () {
 shopResources.init()
 
 let shop = (function () {
-    let amount = 0;
+
+    let amountBeer = 0;
+    let amountVine = 0;
+    let amountPepsi = 0;
+
     function radio() {
         let radios = document.querySelectorAll('.selChange');
         for (let i = 0; i < radios.length; i++) {
-        
+
             if (radios[i].checked) {
                 let selectProduct = gsAll('label')[i].textContent;
                 return selectProduct;
@@ -53,21 +57,39 @@ let shop = (function () {
     }
 
     function count() {
-        if(gs('.enter_count_product').value!='') return gs('.enter_count_product').value;
+        if (gs('.enter_count_product').value != '' && gs('.enter_count_product').value > 0) return gs('.enter_count_product').value;
         else alert('Value is invalid');
     }
 
     gs('.add').onclick = function () {
         let selected = radio();
         let countProduct = count();
-        if(gs('.enter_count_product').value!=''){
-            amount += +countProduct; 
-            console.log(amount);
-            gs('.textarea_for_buy').innerHTML += `<span>${selected}: ${countProduct} шт.</span></br>`
-            gs('.enter_count_product').value='';
-            
+        if (gs('.enter_count_product').value != '' && gs('.enter_count_product').value > 0) {
+            // console.log(amount);
+            if (selected == 'beer') {
+                amountBeer += +countProduct;
+                console.log(amountBeer);
+                gs('.forBeerID').textContent = `${selected}: ${amountBeer} шт.`
+            } else if (selected == 'vine') {
+                amountVine += +countProduct;
+                console.log(amountVine);
+                gs('.forVineID').textContent = `${selected}: ${amountVine} шт.`
+            } else if (selected == 'pepsi') {
+                amountPepsi += +countProduct;
+                console.log(amountPepsi);
+                gs('.forPepsiID').textContent = `${selected}: ${amountPepsi} шт.`
+            }
+            // gs('.enter_count_product').value='';
+
         }
     }
     
-    return
+    return {
+        amountBeer: amountBeer,
+        amountVine: amountVine,
+        amountPepsi: amountPepsi
+    }
 }());
+
+
+console.log(shop.amountBeer);
