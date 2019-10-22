5f66704ff769ce3a99a99321dcaf76ee26251b84
kochutyura <kochutyurawork@gmail.com>
2019-10-22 22:11:11 +0300

    save

--- HW.11 (CenZor and Object task)/CenZor/main.js
+++ HW.11 (CenZor and Object task)/CenZor/main.js
@@ -1,5 +1,28 @@
-alert();
-alert('lol');
-alert('1');
-alert('1');
-alert('12');
+let gs = sel => document.querySelector(sel);
+let badWords = ['java', 'lol'];
+let updateUser = (function () {
+    let add = gs('.add');
+    let reset = gs('.reset');
+    function render() {
+        gs('.badWordsBlock').textContent = badWords.join(', ');
+    }
+    add.onclick = function () {
+        let value = gs('.wordIn').value;
+        if (value != '') {
+            badWords.push(value);
+            console.log(badWords);
+        }
+        gs('.wordIn').value = '';
+        render();
+    };
+    reset.onclick = function () {
+        badWords.forEach(index=>{
+            badWords.splice(0, index);
+        })
+        gs('.badWordsBlock').textContent = '';
+    };
+    return {
+        render: render
+    };
+}());
+updateUser;
--- HW.11 (CenZor and Object task)/CenZor/main.ts
+++ HW.11 (CenZor and Object task)/CenZor/main.ts
@@ -1,5 +1,35 @@
-alert();
-alert('lol');
-alert('1');
-alert('1');
-alert('12');
+let gs = sel => document.querySelector(sel);
+
+let badWords: Array<string> = ['java', 'lol'];
+
+let updateUser = (function (): object {
+    let add = gs('.add');
+    let reset = gs('.reset');
+
+    function render():void{
+        gs('.badWordsBlock').textContent = badWords.join(', ');
+    }
+
+    add.onclick = function ():void{
+        let value: string = gs('.wordIn').value;
+        if (value != '') {
+            badWords.push(value);
+            console.log(badWords);
+        }
+        // render();
+    };
+    reset.onclick = function ():void{
+        badWords.forEach(function (i:number):void {
+            badWords.splice(i, 0);
+        });
+        gs('.badWordsBlock').textContent = '';
+    };
+
+    // console.log(typeof function():void{});
+    
+    return {
+        render: render
+    }
+
+}());
+updateUser;
