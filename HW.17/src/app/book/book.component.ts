663f8e6a72b33e6ac4f7d42c3d464aa38c05fef5
kochutyura <kochutyurawork@gmail.com>
2019-11-10 00:43:14 +0200

    0.2.0

--- HW.17/src/app/book/book.component.ts
+++ HW.17/src/app/book/book.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {IPhone} from '../phone.interface';
+import {Phone} from '../phone.model';
 
 @Component({
   selector: 'app-book',
@@ -8,23 +9,51 @@ import {IPhone} from '../phone.interface';
 })
 export class BookComponent implements OnInit, IPhone {
   firstName: string;
-  id: number;
+  id: number = 3;
   lastName: string;
   phone: string;
+
+  firstNameInput: string;
+  lastNameInput: string;
+  numberInput: string;
+  editCheck: boolean = false;
+  thisUSER: IPhone;
   users: IPhone[] = [
     {id: 1, firstName: 'Yura', lastName: 'Kochut', phone: '0975030847'},
     {id: 2, firstName: 'Adolf', lastName: 'Hitler', phone: '0945632986'},
-    {id: 3, firstName: 'Joseph', lastName: 'Stalin', phone: '0635623496'},
+    {id: 3, firstName: 'Joseph', lastName: 'Stalin', phone: '0635623496'}
   ]
+  @ViewChild('modalClose', {static: false}) modalClose;
+  @ViewChild('modalCHECK', {static: false}) modalCHECK;
   constructor() { }
 
   ngOnInit() {
   }
-  // edit(user: IPhone) {
-  //
-  // }
+  edit(user: IPhone) {
+    this.thisUSER = user;
+    this.editCheck = true;
+    this.firstNameInput = this.thisUSER.firstName;
+    this.lastNameInput = this.thisUSER.lastName;
+    this.numberInput = this.thisUSER.phone;
+    this.modalCHECK.nativeElement.click();
+  }
   delete(user: IPhone) {
     this.users = this.users.filter(i => i.id !== user.id);
   }
-
+  addNumber() {
+    this.id += 1;
+    if (this.firstNameInput.trim() && this.lastNameInput.trim() &&  this.numberInput.trim()) {
+      const user = new Phone(this.id, this.firstNameInput, this.lastNameInput, this.numberInput);
+      this.users.unshift(user);
+      this.firstNameInput = this.lastNameInput = this.numberInput = '';
+      this.modalClose.nativeElement.click();
+    }
+  }
+  save() {
+    this.thisUSER.firstName = this.firstNameInput;
+    this.thisUSER.lastName = this.lastNameInput;
+    this.thisUSER.phone = this.numberInput;
+    this.editCheck = false;
+    this.modalClose.nativeElement.click();
+  }
 }
--- HW.17/src/app/phone.model.ts
+++ HW.17/src/app/phone.model.ts
@@ -1,11 +1,10 @@
 import {IPhone} from './phone.interface';
 
-class Phone implements IPhone {
+export class Phone implements IPhone {
   constructor(
       public id: number,
       public firstName: string,
       public lastName: string,
       public phone: string
   ) {}
-
 }
